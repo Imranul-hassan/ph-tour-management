42f3cef27701181c2e167580872868bb6dee173e
Akib <imranulhassan3296@gmail.com>
2025-07-18 23:45:05 +0600

    setup complete

---
+++ src/server.ts
@@ -0,0 +1,61 @@
+
+import { Server } from "http"
+import mongoose from "mongoose";
+import app from "./app";
+import { envVars } from "./app/config/env";
+
+
+
+let server: Server;
+
+
+const startServer = async () => {
+    try {
+        await mongoose.connect(envVars.DB_URL)
+        console.log("connected to DB")
+
+        server = app.listen(envVars.PORT, () => {
+            console.log(`Server is listening port ${envVars.PORT}`)
+        })
+    } catch (error){
+        console.log(error)
+    }
+}
+
+startServer()
+
+process.on("SIGTERM",()=>{
+    console.log("SIGTERM signal receive")
+
+    if(server){
+        server.close(()=>{
+            process.exit(1);
+        });
+    }
+    process.exit(1)
+})
+
+process.on("unhandledRejection",(err)=>{
+    console.log("Unhandled Rejection Detected", err)
+
+    if(server){
+        server.close(()=>{
+            process.exit(1);
+        });
+    }
+    process.exit(1)
+})
+
+process.on("uncaughtException",(err)=>{
+    console.log("uncaught Exception detection", err);
+
+    if(server){
+        server.close(()=>{
+            process.exit(1);
+        });
+    }
+    process.exit(1)
+})
+
+
+// throw new Error ("I forgot to handle this local")
