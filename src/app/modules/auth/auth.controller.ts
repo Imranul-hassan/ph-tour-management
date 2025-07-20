408196097fb82b85ddd972074e091c8076f24644
Akib <imranulhassan3296@gmail.com>
2025-07-20 18:25:58 +0600

    implement login route with user authentication

---
+++ src/app/modules/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+import { catchAsync } from "../../utils/catchAsync"
+import { sendResponse } from "../../utils/sendResponse"
+import { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status-codes"
+import { AuthServices } from "./auth.service";
+
+const credentialsLogin = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const loginInfo = await AuthServices.credentialsLogin(req.body)
+
+    sendResponse(res,{
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "User login successfully",
+        data: loginInfo
+    })
+})
+
+export const AuthControllers = {
+    credentialsLogin
+}
