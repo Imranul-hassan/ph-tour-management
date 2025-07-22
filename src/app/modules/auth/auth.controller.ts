d034241303f2feeb0b068fbcbd5f117c3894b3e9
Akib <imranulhassan3296@gmail.com>
2025-07-22 12:32:28 +0600

    Implement token related api

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -3,11 +3,25 @@ import { sendResponse } from "../../utils/sendResponse"
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes"
 import { AuthServices } from "./auth.service";
+import AppError from "../../errorHelpers/AppError";
+import { setAuthCookie } from "../../utils/setCookie";
 
 const credentialsLogin = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const loginInfo = await AuthServices.credentialsLogin(req.body)
 
-    sendResponse(res,{
+    // res.cookie("accessToken", loginInfo.accessToken, {
+    //     httpOnly: true,
+    //     secure: false
+    // })
+
+    // res.cookie("refreshToken", loginInfo.refreshToken, {
+    //     httpOnly: true,
+    //     secure: false
+    // })
+
+    setAuthCookie(res, loginInfo)
+
+    sendResponse(res, {
         success: true,
         statusCode: httpStatus.OK,
         message: "User login successfully",
@@ -16,9 +30,21 @@ const credentialsLogin = catchAsync(async (req: Request, res: Response, next: Ne
 })
 
 const getNewAccessToken = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const tokenInfo = await AuthServices.credentialsLogin(req.body)
+    const refreshToken = req.cookies.refreshToken
+    const tokenInfo = await AuthServices.getNewAccessToken(refreshToken as string)
 
-    sendResponse(res,{
+    if (!refreshToken) {
+        throw new AppError(httpStatus.BAD_REQUEST, "No refresh token received from cookies ")
+    }
+
+    // res.cookie("accessToken", tokenInfo.accessToken, {
+    //     httpOnly: true,
+    //     secure: false
+    // })
+
+    setAuthCookie(res, tokenInfo)
+
+    sendResponse(res, {
         success: true,
         statusCode: httpStatus.OK,
         message: "User login successfully",
