11ea1798ecbc0b1f10989c52eb897631012c3ee7
Akib <imranulhassan3296@gmail.com>
2025-07-26 21:13:31 +0600

    Add division route, service and controller

---
+++ src/app/modules/division/division.service.ts
@@ -0,0 +1,43 @@
+
+import { IDivision } from "./division.interface";
+import { Division } from "./division.model";
+
+const createDivision = async (payload: IDivision) => {
+
+    const existingDivision = await Division.findOne({ name: payload.name });
+    if (existingDivision) {
+        throw new Error("A division with this name already exists.");
+    }
+
+
+    // const baseSlug = payload.name.toLowerCase().split(" ").join("-")
+    // let slug = `${baseSlug}-division`
+
+    // let counter = 0;
+    // while (await Division.exists({ slug })) {
+    //     slug = `${slug}-${counter++}` // dhaka-division-2
+    // }
+
+    // payload.slug = slug;
+
+    const division = await Division.create(payload);
+
+    return division
+};
+
+const getAllDivisions = async () => {
+    const divisions = await Division.find({});
+    const totalDivisions = await Division.countDocuments();
+    return {
+        data: divisions,
+        meta: {
+            total: totalDivisions
+        }
+    }
+};
+const getSingleDivision = async (slug: string) => {
+    const division = await Division.findOne({ slug });
+    return {
+        data: division,
+    }
+};
