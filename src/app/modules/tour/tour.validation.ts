7399275a5ea843b0aa2520339f1d07a27391c1b0
Akib <imranulhassan3296@gmail.com>
2025-07-27 23:30:10 +0600

    Update tour service

--- src/app/modules/tour/tour.validation.ts
+++ src/app/modules/tour/tour.validation.ts
@@ -4,18 +4,6 @@ import { z } from "zod";
 
 export const createTourZodSchema = z.object({
     title: z.string(),
-    // description: z.string(),
-    // location: z.string(),
-    // costFrom: z.number(),
-    // startDate: z.string(),
-    // endDate: z.string(),
-    // tourType: z.string(), // <- changed here
-    // included: z.array(z.string()),
-    // excluded: z.array(z.string()),
-    // amenities: z.array(z.string()),
-    // tourPlan: z.array(z.string()),
-    // maxGuests: z.number(),
-    // minAge: z.number(),
     description: z.string().optional(),
     location: z.string().optional(),
     costFrom: z.number().optional(),
